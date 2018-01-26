79f0bbd05044ef7194851719d3ccb8d420b32d71
Steve Lee <smlee.981@gmail.com>
2018-01-26 16:45:15 -0600

    renamed main contract to better refelct internal names and added simple test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
-var PermissionedAccess = artifacts.require("./PermissionedAccess.sol");
-var RoomBase = artifacts.require("./RoomBase.sol");
-var RoomOwnership = artifacts.require('./RoomOwnership.sol');
 var RoomRenting = artifacts.require('./RoomRenting.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(PermissionedAccess);
-  deployer.deploy(RoomBase);
-  deployer.deploy(RoomOwnership);
   deployer.deploy(RoomRenting);
 };
